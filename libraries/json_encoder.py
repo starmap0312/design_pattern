a6ce56f5a21fee383d3810281b7cc4c792e79c9e
Kuan-Yu Chen <starmap0312@gmail.com>
2016-08-30 16:29:42 +0800

    add json encoder

--- libraries/json_encoder.py
+++ libraries/json_encoder.py
@@ -6,6 +6,9 @@ print("json.dumps: convert a python dict to a string")
 a_str = json.dumps(a_dict)
 print(a_str)
 
+a_str = json.dumps(a_dict, indent=2)
+print(a_str)
+
 print("json.loads: convert a string to a python dict")
 a_dict = json.loads(a_str)
 print(a_dict)
