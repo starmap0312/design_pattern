1d283ed29133809ef8039334716e114541f8f9a1
Kuan-Yu Chen <starmap0312@gmail.com>
2016-09-28 21:13:26 +0800

    add some

--- oop/built_in_fake_objects.java
+++ oop/built_in_fake_objects.java
@@ -1,7 +1,7 @@
 // mocking is evil
-//   the reponsibility of an object might be too complex, which makes mocking difficult
-//     i.e. methods return other objects, which also have methods
-//          when we use a mock for the object, we need all of its methods return valid objects 
+//   if the class is too complex, mocking is difficult and a waste
+//     ex. methods return other objects, which also have methods
+//         when we use a mock for the object, we need all of its methods return valid objects 
 //
 // example: mocking a complex object
 //
@@ -38,15 +38,15 @@ public class Employee {
     private final Region region;
 
     public Employee(String empl, Region dynamo) {
-        this.name = empl;
-        this.region = dynamo;
+        this.name = empl;           // the object's characteristic
+        this.region = dynamo;       // a collaborator
     }
 
-    public Integer salary() {      // this is the method that we want to test
+    public Integer salary() {       // this is the method that we want to test
         return Integer.parseInt(
-            this.region            // the production/fake object need to support these methods/operations
-                .table("employees")
-                .frame()
+            this.region             // the production/fake object need to support these methods/operations
+                .table("employees") // return a Table object
+                .frame()            // return a Frame object
                 .where("name", this.name)
                 .iterator()
                 .next()
@@ -59,21 +59,22 @@ public class Employee {
 // the preparation step for mocking a complex object is difficult
 // the unit test code will be much longer than the client class itself
 //
-// (good design: create fake objects and  and ship them together with client classes)
-
-// create a fake class MkRegion
-
+// the solution:
+//   create fake classes, ex. MkRegion, and ship them together with real classes
+//
+// (good design: use fake objects instead and ship them together with client classes)
+//
 // the test class utilizes the fake object
 public class EmployeeTest {
 
     public void canFetchSalaryFromDynamoDb() {
-        Region region = new MkRegion(  // instantiate the fake object which works on H2Data (in-memory H2 database)
+        Region region = new MkRegion(  // instantiate fake object which works on H2Data (in-memory H2 database)
             new H2Data().with("employees", new String[] {"name"}, new String[] {"salary"})
         );
-        region.table("employees").put( // create the test data, i.e. a table record
+        region.table("employees").put( // insert the testing data into the table
             new Attributes().with("name", "Jeff").with("salary", new AttributeValue().withN(50000))
         );
-        Employee emp = new Employee("Jeff", region); // construct the object to be tested with the fake object
-        assertThat(emp.salary(), equalTo(50000))     // test it's salary() method
+        Employee emp = new Employee("Jeff", region); // instantiate the object to be tested with the fake object
+        assertThat(emp.salary(), equalTo(50000))     // test if it's salary() method matches our expectation
     }
 }
