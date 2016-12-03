ecbe0f0730617cb1395bf53bc8a4bd6a423c000b
Kuan-Yu Chen <starmap0312@gmail.com>
2016-12-03 17:00:19 +0800

    add some

--- code_complete/5_3_design_building_blocks_heuristics.java
+++ code_complete/5_3_design_building_blocks_heuristics.java
@@ -1,7 +1,7 @@
 // - Find Real-world Objects
 // 1) identify object and its attributes (characteristics)
-// 2) determine its public and private parts (waht parts will be visible to other objects)
-//    information hiding: for encapsulation, modularity, and abstraction
+// 2) determine its public and private parts (what parts will be visible to other objects)
+//    i.e. information hiding: for better encapsulation, modularity, and abstraction
 //    identify things likely to change: ex. file format, data type implementation
 //      a) hiding source of changes to localize its effects: loose coupling 
 //      b) hiding complexity to improve design and code readability: ex. class Id instead of int
@@ -13,13 +13,14 @@
 //
 // - Encapsulate Implementation Details
 //
-// - Inherit When Inheritance Simplifies The Design
+// - Inherit when Inheritance Simplifies the Design
 // define a base class that specifies common elements of multiple derived classes
 //   it makes code reused but create coupling subclasses
 // adhere to the Liskov Substitution Principle (LSP)
 //   do not inherit from a base class unless derived class truly "is a" more specific version of base class
 //   subclasses must be usable through base class interface without user needs to know the difference
-//   ex. base class: Account, and derived classes: CheckingAccount, SavingsAccount,
+//   ex. base class: Account
+//       derived classes: CheckingAccount, SavingsAccount,
 // the client code should always program to the base class or interface (not thinking about subclass details)
 //
 // - Information Hiding
@@ -29,28 +30,27 @@
 //       ex. use a named constant MAX_EMPLOYEES to hide 100
 //       ex. use id = NewId() to hide id = ++g_maxId
 //    b) creation of data types: improve code design and readability
-//       ex. use a new class type ID to hide int
+//       ex. create class ID to hide int
 //    c) class design, routine design, and subsystem design
 //
 // - Identify Areas Likely to Change
 // isolate unstable areas so that the effect of a change will be limited to one routine, class, or package
-// 1) Status variables
-//    ex. an enumerated type with values: ErrorType_None, ErrorType_Warning, and ErrorType_Fatal
-//    use access routines rather than checking the variable directly
+// ex. Status variables: an enumerated type with values: ErrorType_None, ErrorType_Warning, and ErrorType_Fatal
+//     use access routines rather than checking the variable directly
 //
 // - Keep Coupling Loose
 // kinds of coupling
 // 1) simple-data-parameter coupling (normal and acceptable)
-//    two modules are coupled if all data passed between them are primitive and through parameter lists
-// 2) simple-object coupling (normal and acceptable)
-//    a module is coupled to an object if it instantiates that object
-// 3) object-parameter coupling (not good)
-//    two modules are coupled if Object1 requires Object2 to pass it an Object3
-//    this coupling is tighter than Object1 requiring Object2 to pass it only primitive data types
-//      because it requires Object2 to know about Object3
+//    Object1 is coupled to Object2 if Object1 passes Object2 only primitive data through a parameter list
+// 2) object-parameter coupling (not too good)
+//    Object1 is coupled to Object2 if Object1 passes Object3 to Object2
+//    this coupling is tighter than Object1 passes Object2 only primitive data types through a parameter list
+//      because it requires Object1 to know about Object3 (Object1 is coupled to Object3 as well)
+// 3) simple-object coupling (normal and acceptable)
+//    Object1 is coupled to Object2 if Object1 instantiates Object2
 // 4) semantic coupling (bad)
-//    one module makes use not of some syntactic element of another module but of some semantic
-//      knowledge of another module's inner workings (not good)
+//    one object makes use not of some syntactic element of another object but of some semantic
+//      knowledge of another object's inner workings
 //   ex.
 //   a) Module1 passes a control flag to Module2 that tells Module2 what to do
 //      this requires Module1 to make assumptions about the internal workings of Module2
