0019133eb6c0ce1dbcec490e99397f958589cfe8
Kuan-Yu Chen <starmap0312@gmail.com>
2016-12-11 17:41:42 +0800

    add more

---
+++ code_complete/11_6_creating_short_names_that_are_readable.java
@@ -0,0 +1,17 @@
+// Abbreviation Guidlines 
+// 1) remove all nonleading vowels
+// ex.
+//   computer vs. cmptr
+//   screen   vs. scrn
+//   apple    vs. appl
+//   integer  vs. intgr
+// 2) use the first letter or first few letters of each word
+// 3) keep the first and last letters of each word
+// 4) remove useless suffixes
+// ex.
+//   -ing, -ed, etc.
+// 5) create names that you can pronounce
+// ex.
+//   xPstn    vs. xPos
+//   ndsCmptg vs. needsComp
+
