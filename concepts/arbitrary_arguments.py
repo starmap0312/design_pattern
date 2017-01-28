030daa298f0981077351a47e9674bdab899e64df
Kuan-Yu Chen <starmap0312@gmail.com>
2017-01-28 11:00:26 +0800

    remove

--- concepts/arbitrary_arguments.py
+++
@@ -1,7 +0,0 @@
-# write a function that accepts arbitrary number of arguments
-
-def func(*args):
-  print("arguments: {}".format(args))
-
-func("Hello,", 123, " is one, two, three.")
-
--- libraries/config_parser.py
+++
@@ -1,9 +0,0 @@
-import configparser
-
-parser = configparser.ConfigParser()
-parser._interpolation = configparser.ExtendedInterpolation()
-parser.read("example.ini")
-
-print(parser.sections())
-print(parser.get('SectionTwo', 'Param1'))
-print(parser.get('SectionThree', 'Charlie'))
